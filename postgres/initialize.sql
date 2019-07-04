a4b495fb85f947b7b37454053cd48109267414f2
task320@outlook.com <task320@outlook.com>
2019-07-04 21:09:47 +0900

    初期化時SQL修正

--- postgres/initialize.sql
+++ postgres/initialize.sql
@@ -1,6 +1,7 @@
 CREATE schema test_aspdotnetcore_mvc;
 ALTER DATABASE test_aspdotnetcore_mvc SET timezone TO 'Asia/Tokyo';
 ALTER database test_aspdotnetcore_mvc SET search_path TO test_aspdotnetcore_mvc;
+SELECT pg_reload_conf();
 create table test_aspdotnetcore_mvc."Content" (
   "Id" serial
   , "Name" varchar(256)
